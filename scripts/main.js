a31af836a107c0202ca61b7c6779f415c29a0dcb
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2021-06-17 20:12:15 +0300

    fix: delete  swiper effect for remove horisontal scroll

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,10 @@
 
 // Инициализируем Swiper
 const swiper = new Swiper('.swiper-container', {
-  
-  effect: 'flip',
-  flipEffect: {
-    slideShadows: false,
-  },
+  // effect: 'flip',
+  // flipEffect: {
+  //   slideShadows: false,
+  // },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
