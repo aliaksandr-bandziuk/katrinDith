fe05ba1a862b2ef70deac06fb5911bf86e3b0aa1
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2021-06-18 19:10:03 +0300

    feat: create mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -117,3 +117,14 @@ const enableScroll = () => {
   })
 
 }
+
+// mobile menu
+const mobileMenu = document.querySelector('.mobile-menu'),
+      mobileMenuList = document.querySelector('.mobile-menu__list');
+
+const toggleMenu = () => mobileMenu.addEventListener('click', () => {
+  mobileMenu.classList.toggle('mobile-menu__close');
+  mobileMenuList.classList.toggle('mobile-menu__list-hidden');
+})
+
+toggleMenu();
