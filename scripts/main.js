6b659b9b811298adaf022f114212a469ae8fe73f
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2021-07-09 01:13:06 +0300

    feat: add php form

--- scripts/main.js
+++ scripts/main.js
@@ -128,3 +128,32 @@ const toggleMenu = () => mobileMenu.addEventListener('click', () => {
 })
 
 toggleMenu();
+
+
+// Form
+// const form =  document.getElementById('form');
+// const formButton = document.querySelector('.form-button');
+// console.log('formButton: ', formButton);
+
+// form.addEventListener('submit', formSend);
+
+//   async function formSend(e) {
+//     e.preventDefault();
+
+//     let error = formValidate(form);
+
+//     const formValidate = (form) => {
+
+//       let error = 0;
+//       let formReq = document.querySelectorAll('._req');
+
+//       for (let index = 0; index < formReq.length; index++) {
+//         const input = formReq[index];
+        
+
+        
+//       }
+
+//     }
+
+//   }
